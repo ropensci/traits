c02d9ad5a037e68a1a437d10a59e65131c711655
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-24 08:03:42 -0800

    parsing done on data elements to data.frame, #2, not sure this is best

--- R/traitbank.R
+++ R/traitbank.R
@@ -7,7 +7,10 @@
 #' @references \url{http://eol.org/info/516}
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @examples \donttest{
-#' traitbank(trait = 328574)
+#' res <- traitbank(trait = 328574)
+#' res$context
+#' names( res$graph )
+#' head( res$graph )
 #' }
 
 #' @export
@@ -15,14 +18,26 @@
 traitbank <- function(trait, cache_ttl = NULL, ...)
 {
   args <- traitsc(list(cache_ttl=cache_ttl))
-  traitbank_GET(paste0(tburl(), trait), args, ...)
+  res <- traitbank_GET(paste0(tburl(), trait), args, ...)
+  df <- res$`@graph`
+  meas <- unlist(sapply(df$`dwc:measurementValue`, function(x){
+    if(length(x) == 0){
+      NA
+    } else {
+      if(length(x) > 1) x[[1]][[1]] else x
+    }
+  }))
+  meas_id <- sapply(df$`dwc:measurementValue`, function(x) if(length(x) > 1) x$`@id` else NA)
+  df <- data.frame(df, `dwc:measurementValue` = meas, `dwc:measurementValue.id` = meas_id, stringsAsFactors = FALSE)
+  df$`dwc:measurementValue` <- NULL
+  structure(list(context=res$`@context`, graph=df), class="traitbank")
 }
 
 traitbank_GET <- function(url, args = list(), ...){
   res <- GET(url, query = args, ...)
   stop_for_status(res)
   txt <- content(res, "text")
-  jsonlite::fromJSON(txt, FALSE)
+  jsonlite::fromJSON(txt, TRUE, flatten = TRUE)
 }
 
 tburl <- function() 'http://eol.org/api/traits/'
