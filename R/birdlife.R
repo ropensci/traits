0e87824ca5f3b7701a8e0fdc0f0633bc72c6b5fc
davharris <harry491@gmail.com>
2015-05-28 14:13:58 -0700

    preliminary birdlife threats

--- R/birdlife.R
+++ R/birdlife.R
@@ -25,3 +25,46 @@ birdlife_habitat = function(id){
   out = cbind(id, tables[[habitat_table_number]])
   out[-nrow(out), ] # Drop last row (altitude)
 }
+
+
+birdlife_threats = function(id){
+
+  url = paste0(
+    "http://www.birdlife.org/datazone/species/factsheet/",
+    id,
+    "/additional"
+  )
+
+  tables = XML::readHTMLTable(url, stringsAsFactors = FALSE)
+
+  is_threats = sapply(
+    tables,
+    function(x){
+      all(c("Scope", "Severity", "Impact", "Timing") %in% unlist(x))
+    }
+  )
+
+  if(sum(is_threats) > 1){
+    stop("Malformed input. Multiple threat tables in ID ", id)
+  }
+
+  if(sum(is_threats) == 1){
+    threats = tables[is_threats][[1]]
+
+    rownums = seq_len(nrow(threats))
+
+    out = data.frame(
+      threat1  = threats[rownums %% 5 == 1, 1],
+      threat2  = threats[rownums %% 5 == 1, 2],
+      stresses = threats[rownums %% 5 == 0, 1],
+      timing   = threats[rownums %% 5 == 2, 1],
+      scope    = threats[rownums %% 5 == 2, 2],
+      severity = threats[rownums %% 5 == 2, 3],
+      impact   = threats[rownums %% 5 == 2, 4]
+    )
+  }else{
+    out = NULL
+  }
+
+  out
+}
