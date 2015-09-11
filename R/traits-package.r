a321c21e95ac2a8a494dde45005f27e89324b278
Scott Chamberlain <myrmecocystus@gmail.com>
2015-09-11 12:02:27 -0700

    added @davharris as author

--- R/traits-package.r
+++ R/traits-package.r
@@ -1,4 +1,31 @@
-#' traits
+#' traits - Species trait data from around the web
+#'
+#' Currrently included in \code{traits} with the associated function prefix:
+#' \itemize{
+#'  \item BETYdb \url{http://www.betydb.org} - \code{betydb_}
+#'  \item National Center for Biotechnology Information - NCBI
+#'  \url{http://www.ncbi.nlm.nih.gov/} - \code{ncbi_}
+#'  \item Global Invasive Species Database - GISD \url{http://www.issg.org/database/welcome/}
+#'  - \code{g_}
+#'  \item Encyclopedia of Life Invasive Species - \code{eol_}
+#'  \item Encyclopedia of Life Traitbank - \code{traitbank_}
+#'  \item Coral Traits Database \url{http://coraltraits.org/} - \code{coral_}
+#'  \item Flora Europaea \url{http://rbg-web2.rbge.org.uk/FE/fe.html} - \code{fe_}
+#'  \item Birdlife International \url{http://rbg-web2.rbge.org.uk/FE/fe.html} - \code{birdlife_}
+#'  \item LEDA Traitbase \url{http://www.leda-traitbase.org/LEDAportal/index.jsp} - \code{leda_}
+#'  \item More to come ...
+#' }
+#'
+#' @examples \dontrun{
+#' library("traits")
+#'
+#' ## Search the Coral database
+#' ## Get the species list and their ids
+#' coral_species()
+#'
+#' ## Get data by taxon
+#' coral_taxa(80)
+#' }
 #'
 #' @importFrom methods is
 #' @importFrom stats setNames na.omit
@@ -6,10 +33,10 @@
 #' @name traits-package
 #' @aliases traits
 #' @docType package
-#' @title Species trait data from around the web
 #' @author Ignasi Bartomeus \email{nacho.bartomeus@@gmail.com}
 #' @author Zachary Foster \email{zacharyfoster1989@@gmail.com}
 #' @author David LeBauer \email{dlebauer@@gmail.com}
+#' @author David Harris \email{davharris@@ucdavis.edu}
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @keywords package
 NULL
