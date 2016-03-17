e741f73632541ab4849ad7e60e05423fc94b1fbf
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-17 12:05:40 -0700

    remove links to fxns in originr

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,13 +4,13 @@
 #'
 #' \itemize{
 #'  \item \code{\link{eol_invasive_}}: This function is moving to a new package to
-#'  be on CRAN soon. It will be \code{\link{eol}} in \code{originr} package
+#'  be on CRAN soon. It will be \code{eol} in \code{originr} package
 #'  \item \code{\link{fe_native}}: This function is moving to a new package to
-#'  be on CRAN soon. It will be \code{\link{flora_europaea}} in \code{originr} package
+#'  be on CRAN soon. It will be \code{flora_europaea} in \code{originr} package
 #'  \item \code{\link{g_invasive}}: This function is moving to a new package to
-#'  be on CRAN soon. It will be \code{\link{gisd}} in \code{originr} package
+#'  be on CRAN soon. It will be \code{gisd} in \code{originr} package
 #'  \item \code{\link{is_native}}: This function is moving to a new package to
-#'  be on CRAN soon. It will be \code{\link{is_native}} in \code{originr} package
+#'  be on CRAN soon. It will be \code{is_native} in \code{originr} package
 #' }
 #'
 #' @name traits-deprecated
