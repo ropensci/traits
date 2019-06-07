e56752e3475ce2249cdf98d95f3ea4f840544ff4
Scott Chamberlain <myrmecocystus@gmail.com>
2019-06-07 11:22:24 -0700

    skip traitbank tests on cran

--- tests/testthat/test-traitbank.R
+++ tests/testthat/test-traitbank.R
@@ -1,37 +1,6 @@
-# tbtest_df <- data.frame(
-#   sciname = c("Balaenoptera musculus", "Poa annua", "Closterocerus formosus"),
-#   pageid = c(328574, 1114594, 846827))
-# tbtest_n <- dim(tbtest_df)[1]
-
-# test_that("Traitbank API works", {
-#   skip_on_cran()
-#   # check_traitbank()
-
-#   for (k in seq_len(tbtest_n)) {
-#     get.out <- traitbank_GET(paste0(tburl(), tbtest_df[k, "pageid"]))
-#     expect_is(get.out, "list", info = tbtest_df[k, "sciname"])
-#     expect_true("item" %in% names(get.out), info = tbtest_df[k, "sciname"])
-#     expect_true("@context" %in% names(get.out), info = tbtest_df[k, "sciname"])
-#     expect_true("traits" %in% names(get.out$item), info = tbtest_df[k, "sciname"])
-#   }
-# })
-
-# test_that("Basic search works", {
-#   skip_on_cran()
-#   # check_traitbank()
-
-#   for (k in seq_len(tbtest_n)) {
-#     get.out <- traitbank_GET(paste0(tburl(), tbtest_df[k, "pageid"]))
-#     expect_equal(get.out$item$`@id`, tbtest_df[k, "pageid"],
-#       info = tbtest_df[k, "sciname"])
-#     expect_equal(get.out$item$`dwc:taxonRank`, "species",
-#       info = tbtest_df[k, "sciname"])
-#     expect_true(grepl(tbtest_df[k, "sciname"], get.out$item$scientificName),
-#       info = tbtest_df[k, "sciname"])
-#   }
-# })
-
 test_that("traitbank", {
+  skip_on_cran()
+
   aa <- traitbank(query = "MATCH (n:Trait) RETURN n LIMIT 1;")
 
   expect_is(aa, "list")
@@ -52,5 +21,3 @@ test_that("traitbank: fails well", {
   expect_error(traitbank(), "\"query\" is missing", class = "error")
   expect_error(traitbank("asfasfd"), "Forbidden", class = "error")
 })
-
-# rm(tbtest_df, tbtest_n)
